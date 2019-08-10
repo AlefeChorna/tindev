f985b3a80194bd23901197b6d142c0f81dc51bd0
Aléfe Chorna <alefechorna99@gmail.com>
2019-08-10 00:27:54 -0300

    Criado servidor da aplicação TinDev

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res) {
+    const { devId } = req.params;
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) {
+      return res.status(400).json({ error: 'Dev not exists' })
+    }
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      console.log('Deu match hahaha');
+    }
+
+    if (loggedDev.likes.includes(targetDev._id)) {
+      return res.json(loggedDev);
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect(
+  'mongodb+srv://your_username:our_password@cluster0-pp5yf.mongodb.net/timdev?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
